a683a0d306ca4b3192cc18da0717a379eaba70b4
Deniz Dogan <denizdogan@users.noreply.github.com>
2018-01-19 22:11:38 +0100

    Add throttling on 'damp' module and add prettier

--- src/modules/damp.js
+++ src/modules/damp.js
@@ -1,8 +1,16 @@
 const debug = require('debug')('nima:modules:damp')
+const moment = require('moment')
+
+let lastTime = null
+const MIN_TIME_DIFF = 60 // seconds
 
 module.exports = async function(msg) {
   const { content } = msg
   if (content.includes('damp')) {
-    msg.channel.send('fuck detta livet ursäkta mitt språk men ibland får man damp')
+    // if never sent or sent more than MIN_TIME_DIFF seconds ago
+    if (lastTime === null || moment.diff(lastTime, 'seconds', true)) {
+      msg.channel.send('fuck detta livet ursäkta mitt språk men ibland får man damp')
+      lastTime = moment.now()
+    }
   }
 }
