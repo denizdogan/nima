545c94a8eb3eba89dac15ec361f2d62069132395
Deniz Dogan <denizdogan@users.noreply.github.com>
2018-01-19 22:24:06 +0100

    Fix: The time limit in damp.js didn't work at all

--- src/modules/damp.js
+++ src/modules/damp.js
@@ -8,8 +8,11 @@ module.exports = async function(msg) {
   const { content } = msg
   if (content.includes('damp')) {
     // if never sent or sent more than MIN_TIME_DIFF seconds ago
-    if (lastTime === null || moment.diff(lastTime, 'seconds', true)) {
+    debug('lastTime = %s', lastTime)
+    if (lastTime === null || moment().diff(lastTime, 'seconds', true) > MIN_TIME_DIFF) {
       msg.channel.send('fuck detta livet ursäkta mitt språk men ibland får man damp')
+      debug('lastTime is now %s', lastTime)
+      debug('diff is %s', moment().diff(lastTime, 'seconds', true))
       lastTime = moment.now()
     }
   }
