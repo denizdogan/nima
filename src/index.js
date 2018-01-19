f3ce430f91c0ca8048c105ffaf78105b01896d7c
Barberdad <sebastian.rasimus@gmail.com>
2018-01-20 00:28:54 +0100

    Added a module for saving and displaying quotes

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ const calc = require('./modules/calc')
 const convert = require('./modules/convert')
 const damp = require('./modules/damp')
 const ping = require('./modules/ping')
+const quote = require('./modules/quote')
 
 const client = new Discord.Client()
-const MODULES = [calc, convert, damp, ping]
+const MODULES = [calc, convert, damp, ping, quote]
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
---
+++ src/modules/quote.js
@@ -0,0 +1,100 @@
+require('dotenv-safe').load()
+
+const debug = require('debug')('nima:modules:quote')
+const fs = require('fs')
+const parseInt = require('parse-int')
+const commands = {'!qsearch': quoteSearch,
+                  '!addquote': addQuote,
+                  '!randquote': randQuote,
+                  '!quote': showQuote}
+module.exports = async function(msg) {
+    const command = msg.content.split(' ')[0];
+    commands[command](msg);
+}
+
+function addQuote(msg) {
+    const { content, guild } = msg
+    const file = `${process.env.QUOTES_PATH}/${guild.id}.json`
+
+    fs.readFile(file, (err, data) => {
+        if (content.startsWith('!addquote ')) {
+            
+            const quote = content.replace('!addquote ', '')
+            if (data === undefined) {
+                //Create new array of quotes if none exists
+                quotes = []
+            } else {
+                quotes = JSON.parse(data)
+            }
+            quotes.push(quote)
+            //Write json to file
+            fs.writeFile(file, JSON.stringify(quotes), function(err){
+                if (err) throw err
+            })
+        }
+    })
+}
+function showQuote(msg) {
+    const { content, guild } = msg
+    const file = `${process.env.QUOTES_PATH}/${guild.id}.json`
+    const quoteId = parseInt(content.replace('!quote ', ''))
+    if (quoteId === undefined) {
+        msg.reply('Please use the format: !quote quote-id')
+        return
+    }
+    fs.readFile(file, (err, data) => {
+        if (data === undefined) {
+            msg.reply('No quotes found. Sorry!')
+        } else {
+            quotes = JSON.parse(data)
+            //Reply with quote
+            if (quoteId >= quotes.length) {
+                msg.reply(`There is no quote ${quoteId}`)
+            } else {
+                msg.reply(`Quote ${quoteId}\n${quotes[quoteId]}`)
+            }
+        }
+    })
+}    
+
+//Show a random quote
+function randQuote(msg) {
+    const { content, guild } = msg
+    const file = `${process.env.QUOTES_PATH}/${guild.id}.json`
+    fs.readFile(file, (err, data) => {
+        if (data === undefined) {
+            msg.reply('No quotes found. Sorry!')
+        } else {
+            quotes = JSON.parse(data)
+            //Reply with a random quote
+            const n = Math.floor(Math.random() * quotes.length)
+            msg.reply(`Quote ${n}\n${quotes[n]}`)
+        }
+    })
+}
+//Show a quote matching some key-phrase
+function quoteSearch(msg) {
+    const { content, guild } = msg
+    const file = `${process.env.QUOTES_PATH}/${guild.id}.json`
+    fs.readFile(file, (err, data) => {
+        const key = content.replace('!qsearch ', '')
+        if (data === undefined) {
+            msg.reply('No quotes found.')        
+        } else {
+            quotes = JSON.parse(data)
+            //Finding all matches (not necessary but might be useful in the future)
+            let matches = []
+            quotes.forEach((quote, index) => {
+                if (quote.includes(key)) {
+                    matches.push(index)
+                }
+            })
+            //Reply with the first matching quote
+            if (!matches[0]) {
+                msg.reply('No quotes found.')
+            } else {
+                msg.reply(`Quote ${matches[0]}\n${quotes[matches[0]]}`)
+            }
+        }
+    })
+}
