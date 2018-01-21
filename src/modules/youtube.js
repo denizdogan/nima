370bc12025aad00511292d4cc6f04c7348e755ef
Deniz Dogan <denizdogan@users.noreply.github.com>
2018-01-21 22:41:45 +0100

    Feature: Add !yt as an alias to !youtube

--- src/modules/youtube.js
+++ src/modules/youtube.js
@@ -7,7 +7,7 @@ const promiseSearch = Promise.promisify(youtubeSearch)
 
 export default async function(msg) {
   const [command, ...tail] = msg.content.split(' ')
-  if (command !== '!youtube') {
+  if (!['!youtube', '!yt'].includes(command)) {
     return
   }
 
